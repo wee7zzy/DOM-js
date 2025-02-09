39138d904489a94a1f7e325fd3148be0b078b595
yassine ELANIQ <yassaniq@gmail.com>
2025-02-10 00:18:37 +0100

    challenge 2

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const btn1=document.getElementById("btn1");
 const para=document.getElementById("p1");
 
+const btn2=document.getElementById("btn2");
+const HE1=document.getElementById("h1");
+
 btn1.addEventListener("click",()=>{
     para.innerHTML="hello , i'm yassine";
+})
+
+let i=0;
+btn2.addEventListener('click',()=>{
+    i++;
+    HE1.textContent=i;
 })
 
 
