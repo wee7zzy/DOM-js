c395f240fc533d6e555f045afec015ce0d3ce8d8
yassine ELANIQ <yassaniq@gmail.com>
2025-02-11 20:17:19 +0100

    challenge 4 couleur de fond de page

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ const HE1=document.getElementById("h1");
 
 const btn3=document.getElementById('btn3');
 let input1=document.getElementById("inputmaj");
-// let uppertext=input.toUppercase();
+
+const btnred=document.getElementById('btnred');
+const btnbleu=document.getElementById('btnbleu');
+const btnvert=document.getElementById('btnvert');
+const backg=document.getElementById("chal4");
+
 
 btn1.addEventListener("click",()=>{
     para.innerHTML="hello , i'm yassine";
@@ -23,6 +28,16 @@ btn3.addEventListener('click',()=>{
    let uppertext= input1.value.toUpperCase();
     
     document.getElementById('resu').innerText='resultat : '+ uppertext;
+})
+
+btnred.addEventListener("click",()=>{
+    backg.style.backgroundColor="red";
+})
+btnbleu.addEventListener("click",()=>{
+    backg.style.backgroundColor="blue";
+})
+btnvert.addEventListener("click",()=>{
+    backg.style.backgroundColor="green";
 })
 
 
