6e750c3417556f3726c0a9feaa1f3fd38ca00a6b
yassine ELANIQ <yassaniq@gmail.com>
2025-02-11 20:03:06 +0100

    challenge afficher un text en majuscule

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const para=document.getElementById("p1");
 const btn2=document.getElementById("btn2");
 const HE1=document.getElementById("h1");
 
+const btn3=document.getElementById('btn3');
+let input1=document.getElementById("inputmaj");
+// let uppertext=input.toUppercase();
+
 btn1.addEventListener("click",()=>{
     para.innerHTML="hello , i'm yassine";
 })
@@ -12,6 +16,13 @@ let i=0;
 btn2.addEventListener('click',()=>{
     i++;
     HE1.textContent=i;
+}) 
+
+
+btn3.addEventListener('click',()=>{
+   let uppertext= input1.value.toUpperCase();
+    
+    document.getElementById('resu').innerText='resultat : '+ uppertext;
 })
 
 
